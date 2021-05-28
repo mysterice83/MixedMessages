bc556fb46b589f875dff02ed05076bbf368b97da
mysterice83 <84747336+mysterice83@users.noreply.github.com>
2021-05-27 19:39:06 -0700

    Update MixedMessages.js
    added a Scooby Doo character

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,7 +1,7 @@
 // Week 6 Group Project: Mixed Messages by tinisha, oscar, michelle, julio, imani
 
 const suspectedElements = {
-	whoDoneIt: ['Miss Scarlett', 'Professor Plum', 'Mrs. Peacock', 'Colonel Mustard', 'Mrs. White', 'Mr. Green'],
+	whoDoneIt: ['Miss Scarlett', 'Professor Plum', 'Scrappy Dappy Doo', 'Colonel Mustard', 'Mrs. White', 'Mr. Green'],
 	whereItDone: ['ballroom', 'billiard room', 'conservatory', 'dining room', 'hall', 'kitchen', 'library', 'lounge', 'study'],
 	whatDoneIt: ['candlestick', 'knife', 'lead pipe', 'revolver', 'rope', 'wrench']
 };
